90ceb84e7aa4e1348de99772d55f12337fb022a7
Praveen <prawin20@gmail.com>
2018-03-08 16:07:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cros());
 
 app.use(function(req,res,next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 	
